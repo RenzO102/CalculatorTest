1a7e16ad63bea362047d71ecd67829d7eae9cd01
RenzO102 <109575926+RenzO102@users.noreply.github.com>
2022-09-21 18:23:28 +0500

    Update Calculator.java

--- CalculactorTest/src/main/java/ru/github/com/RenzO102/Calculator.java
+++ CalculactorTest/src/main/java/ru/github/com/RenzO102/Calculator.java
@@ -1,12 +1,10 @@
 package ru.github.com.RenzO102;
 
 public class Calculator {
-    public Object evaluate1(String y) {
+    public Object evaluate1(String s) {
+        Calculator calc = new Calculator;
+        calc.equals(s);
         return 5;
-    }
-
-    public Object evaluate2(String y) {
-        return 61;
     }
 }
 
