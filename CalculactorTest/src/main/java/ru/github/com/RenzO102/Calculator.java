e71dcb82c7b7e42172005a7beeb736170ea5f89c
RenzO102 <109575926+RenzO102@users.noreply.github.com>
2022-09-26 13:11:24 +0500

    Update Calculator.java

--- CalculactorTest/src/main/java/ru/github/com/RenzO102/Calculator.java
+++ CalculactorTest/src/main/java/ru/github/com/RenzO102/Calculator.java
@@ -1,13 +1,8 @@
- public int sum(int a, int b) {
-
-
+package ru.github.com.RenzO102;
+    public int sum(int a, int b) {
         return a + b;
     }
-
     public int minus(int a, int b) {
-
-
         return a - b;
     }
 }
-
